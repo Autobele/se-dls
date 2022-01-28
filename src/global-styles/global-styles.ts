bbc499aa0eae23d41c9eb2efa5d5693f7e8091d0
Autobele <autobele@infoway-pi.com.br>
2022-01-28 00:01:03 -0300

    Add font Roboto

--- src/global-styles/global-styles.ts
+++ src/global-styles/global-styles.ts
@@ -1,13 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+      font-family: 'Roboto';
+      src: url('https://fonts.gstatic.com/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2') format('woff2');
+    }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Roboto';
   }
 
   h1, h2, h3, h4, h5, h6 {
+    font-family: 'Roboto';
     margin: 0;
   }
+
+  
 `
